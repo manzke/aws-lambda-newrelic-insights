e53a43fb32fc432d6a6a5c7c9f63578b8cb4f4ef
Daniel Manzke <daniel.manzke@googlemail.com>
2015-11-19 23:38:25 +0100

    moved kinesis to insights pusher as well

--- insights-pusher-kinesis.js
+++ insights-pusher-kinesis.js
@@ -1,14 +1,7 @@
 var superagent = require('superagent');
+var pusher = require('insights-pusher');
 
-var baseURL = 'https://insights-collector.newrelic.com/v1/accounts/';
-var endpoint = '/events';
-var authHeader = 'X-Insert-Key';
-
-function constructURL(accountId) {
-    return baseURL + accountId + endpoint;
-}
-
-console.log('Loading function insights-pusher.js');
+console.log('Loading function insights-pusher-kinesis.js');
 
 exports.handler = function(event, context) {
     event.Records.forEach(function(record, index, array) {
@@ -17,31 +10,17 @@ exports.handler = function(event, context) {
         console.log('Decoded payload:', event);
         event = JSON.parse(event);
         
-        var apiKey = event.apiKey;
-        var accountId = event.accountId;
-        var insightEvent = event.item;
-        
-        console.log('apiKey = ' + apiKey);
-        console.log('accountId = ' + accountId);
-        console.log('insightEvent = ' + insightEvent);
-
-        var insightsURL = constructURL(accountId);
-        console.log('insightsURL = ' + insightsURL);
-    
-        superagent.post(insightsURL)
-                .set(authHeader, apiKey)
-                .send(insightEvent)
-                .end(function(error, response) {
-                        console.log(error);
-                        if (response.ok) {
-                            //foreach is synchronous so the last event should only succeed
-                            if (index === array.length - 1) {
-                                context.succeed(response);
-                            }
-                        } else {
-                            //fail fast
-                            context.fail(error);
+        pusher.push(function(error) {
+                        //fail fast
+                        context.fail(error);
+                    },
+                    function(response) {
+                        //foreach is synchronous so the last event should only succeed
+                        if (index === array.length - 1) {
+                            context.succeed(response);
                         }
-                    });
+                    },
+                    event, context
+        );
     });
 };
